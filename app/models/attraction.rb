390632af896bf7eb548dcdc4dcba2edc0f0e853d
DKendle <dina.kendle@gmail.com>
2020-09-16 07:53:27 -0400

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,4 @@
 class Attraction < ApplicationRecord
+    has_many :users
+    has_many :rides, through: :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :rides, through: :attractions
+    has_many :attractions
 end
